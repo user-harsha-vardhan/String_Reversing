0e724f1d83f89fb71b0400133b519b514655161d
Harshavardhan <harshagowdaout987@gmail.com>
2025-08-26 10:31:17 +0530

    fun reverse string added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,19 @@
-fun main () {
-   println("Enter string to reverse:")
+fun main() {
+    println("Enter string to reverse:")
     val inputString = readln()
 
+    val reverseString = reverseString(inputString)
+    if (reverseString == inputString)
+    println("this is a palindrome")
+    else  println("The reversed string is: ${reverseString(inputString)}")
+
+}
+
+fun reverseString(reverse: String): String {
     val finalString = buildString {
-        for (i in inputString.lastIndex downTo 0) {
-            append(inputString[i])
+        for (i in reverse.lastIndex downTo 0) {
+            append(reverse[i])
         }
     }
-    println("The reversed string is: $finalString")
+    return finalString
 }
